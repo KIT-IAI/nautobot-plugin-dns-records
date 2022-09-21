85d03d1eb34b0b93c14f6084932a7997e8a5a6e7
Daniel Bacher <mail@phill93.de>
2022-09-21 14:39:24 +0200

    disable max version directive

--- nautobot_dns_records/__init__.py
+++ nautobot_dns_records/__init__.py
@@ -11,7 +11,7 @@ class NautobotDnsRecordsConfig(PluginConfig):
     base_url = "dns"  # (Optional) Base path to use for plugin URLs. Defaulting to app_name.
     required_settings = []  # A list of any configuration parameters that must be defined by the user.
     min_version = "1.4.0"  # Minimum version of Nautobot with which the plugin is compatible.
-    max_version = "1.4.0"  # Maximum version of Nautobot with which the plugin is compatible.
+    # max_version = "1.4.0"  # Maximum version of Nautobot with which the plugin is compatible.
     default_settings = {}  # A dictionary of configuration parameters and their default values.
     caching_config = {}  # Plugin-specific cache configuration.
     author = "Daniel Bacher"
