59a92b953e6a7dec2f667014d8aefc4c27d94604
Daniel Bacher <mail@phill93.de>
2024-04-03 11:18:21 +0200

    fix invalid filters

--- nautobot_dns_records/views/device_tab.py
+++ nautobot_dns_records/views/device_tab.py
@@ -31,9 +31,9 @@ class DeviceRecordsTab(generic.ObjectView):
 
     def get_extra_context(self, request, instance):  # pylint: disable-msg=too-many-locals
         """Returns all dns records related to a device."""
-        addressrecords = AddressRecord.objects.filter(address__interface__device=instance).all()
+        addressrecords = AddressRecord.objects.filter(address__interfaces__device_id=instance.id).all()
         addressrecords_table = AddressRecordTable(data=addressrecords, user=request.user, orderable=False)
-        ptrrecords = PtrRecord.objects.filter(address__interface__device=instance).all()
+        ptrrecords = PtrRecord.objects.filter(address__interfaces__device_id=instance.id).all()
         ptrrecords_table = PtrRecordTable(data=ptrrecords, user=request.user, orderable=False)
         txtrecords = TxtRecord.objects.filter(device=instance).all()
         txtrecords_table = TxtRecordTable(data=txtrecords, user=request.user, orderable=False)
