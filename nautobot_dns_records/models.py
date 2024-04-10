86af26e43f6e44d36f8e39a0fccdee7a1e562776
Daniel Bacher <mail@phill93.de>
2024-04-10 10:53:03 +0200

    reformat with black

--- nautobot_dns_records/models.py
+++ nautobot_dns_records/models.py
@@ -7,6 +7,7 @@ import nautobot.ipam.models
 from django.core.validators import MaxValueValidator, MinValueValidator, RegexValidator
 from django.db import models
 from django.db.models.constraints import UniqueConstraint
+
 # from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 from nautobot.core.models.generics import PrimaryModel
